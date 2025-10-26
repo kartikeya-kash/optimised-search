a6a2774a4989a8bdc90671a9b3212d8a422d587e
Kartikeya <kartikeya.anjul@gmail.com>
2025-10-26 20:51:35 +0530

    created server

---
+++ src/server.js
@@ -0,0 +1,36 @@
+import express from "express";
+import mysql from "mysql2";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "searchopti",
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error("❌ Database connection failed:", err);
+    return;
+  }
+  console.log("✅ Connected to MySQL database!");
+});
+
+//get data
+app.get("/data", (req, res) => {
+  const q = "SELECT * FROM data"; 
+  db.query(q, (err, data) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
+    res.json(data);
+  });
+});
+
+const PORT =5000;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
